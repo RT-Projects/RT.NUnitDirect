d920997aaf34fd7356536dbbe2f3e11fa8c5363b
rstarkov <sourcecode@roman.st>
2010-02-12 22:48:06 +0000

    A library to invoke NUnit tests in such a way that the exceptions aren't suppressed. Compatible with NUnit 2.5.3 only. (imported from Perforce changelist 1584)

---
+++ Src/Direct.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using NUnit.Core;
+using RT.Util;
+using RT.Util.ExtensionMethods;
+
+namespace NUnit.Direct
+{
+    public static class NUnitDirect
+    {
+        public static void RunTestsOnAssembly(Assembly assembly)
+        {
+            var package = new TestPackage(assembly.Location);
+            if (!CoreExtensions.Host.Initialized)
+                CoreExtensions.Host.InitializeService();
+
+            var testsIndirect = new TestSuiteBuilder().Build(package);
+            var tests = directize(testsIndirect);
+
+            var results = new TestResult(tests);
+            tests.Run(results, new DirectListener(), NUnit.Core.TestFilter.Empty);
+        }
+
+        private static DirectTestSuite directize(TestSuite suite)
+        {
+            var result = new DirectTestSuite(suite);
+
+            // replace all TestSuite's with MyTestSuite's recursively... ugh...
+            Queue<Test> tests = new Queue<Test>();
+            tests.Enqueue(result);
+            while (tests.Count > 0)
+            {
+                var test = tests.Dequeue();
+                if (test.Tests == null)
+                    continue;
+                var subtests = test.Tests.Cast<Test>().ToList();
+                test.Tests.Clear();
+                foreach (var subtest in subtests)
+                {
+                    if (subtest is TestSuite)
+                    {
+                        var replacement = new DirectTestSuite((TestSuite) subtest);
+                        test.Tests.Add(replacement);
+                        tests.Enqueue(replacement);
+                    }
+                    else if (subtest is TestMethod)
+                    {
+                        var replacement = new DirectTestMethod((TestMethod) subtest);
+                        test.Tests.Add(replacement);
+                        tests.Enqueue(replacement);
+                    }
+                    else
+                    {
+                        test.Tests.Add(subtest);
+                        tests.Enqueue(subtest);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+
+    class DirectListener : EventListener
+    {
+        private LoggerBase log = new ConsoleLogger();
+
+        public void RunFinished(Exception exception)
+        {
+            log.Warn("Test run finished with exception:");
+            log.Exception(exception);
+        }
+
+        public void RunFinished(TestResult result)
+        {
+            log.Info(result.IsSuccess ? "Test run finished - SUCCESS" : "Test run finished - FAILURE");
+            // don't care too much about logging failures because they would most certainly never reach this point,
+            // given how exceptions are not swallowed.
+        }
+
+        public void RunStarted(string name, int testCount)
+        {
+        }
+
+        public void SuiteFinished(TestResult result)
+        {
+        }
+
+        public void SuiteStarted(TestName testName)
+        {
+            log.Info("Suite {0}".Fmt(testName.Name));
+        }
+
+        public void TestFinished(TestResult result)
+        {
+        }
+
+        public void TestOutput(TestOutput testOutput)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestStarted(TestName testName)
+        {
+            log.Info("Running test {0} ...".Fmt(testName.Name));
+        }
+
+        public void UnhandledException(Exception exception)
+        {
+            // we don't expect this to ever get invoked
+            log.Warn("Unhandled exception:");
+            log.Exception(exception);
+        }
+    }
+
+    #region This was copied & pasted out of NUnit 2.5.3 where overriding alone wasn't enough. "catch" clauses were removed.
+
+    class DirectTestSuite : TestSuite
+    {
+        public DirectTestSuite(TestSuite suite)
+            : base(suite.TestName.Name)
+        {
+            foreach (var fld in typeof(TestSuite).GetAllFields())
+                fld.SetValue(this, fld.GetValue(suite));
+        }
+
+        protected override void DoOneTimeSetUp(TestResult suiteResult)
+        {
+            if (FixtureType != null)
+            {
+                // In case TestFixture was created with fixture object
+                if (Fixture == null && !(FixtureType.IsAbstract && FixtureType.IsSealed))
+                    CreateUserFixture();
+
+                if (this.Properties["_SETCULTURE"] != null)
+                    TestContext.CurrentCulture =
+                        new System.Globalization.CultureInfo((string) Properties["_SETCULTURE"]);
+
+                if (this.Properties["_SETUICULTURE"] != null)
+                    TestContext.CurrentUICulture =
+                        new System.Globalization.CultureInfo((string) Properties["_SETUICULTURE"]);
+
+                if (this.fixtureSetUpMethods != null)
+                    foreach (MethodInfo fixtureSetUp in fixtureSetUpMethods)
+                        Helper.InvokeMethodDirect(fixtureSetUp, fixtureSetUp.IsStatic ? null : Fixture, new object[0]);
+            }
+        }
+
+        protected override void DoOneTimeTearDown(TestResult suiteResult)
+        {
+            if (this.Fixture != null)
+            {
+                if (this.fixtureTearDownMethods != null)
+                {
+                    int index = fixtureTearDownMethods.Length;
+                    while (--index >= 0)
+                    {
+                        MethodInfo fixtureTearDown = fixtureTearDownMethods[index];
+                        Helper.InvokeMethodDirect(fixtureTearDown, fixtureTearDown.IsStatic ? null : Fixture, new object[0]);
+                    }
+                }
+
+                IDisposable disposable = Fixture as IDisposable;
+                if (disposable != null)
+                    disposable.Dispose();
+
+                this.Fixture = null;
+            }
+        }
+    }
+
+    class DirectTestMethod : TestMethod
+    {
+        public DirectTestMethod(TestMethod other)
+            : base(other.Method)
+        {
+            foreach (var fld in typeof(TestMethod).GetAllFields())
+                fld.SetValue(this, fld.GetValue(other));
+        }
+
+        public override void doRun(TestResult testResult)
+        {
+            DateTime start = DateTime.Now;
+
+            try
+            {
+                doSetUp();
+
+                doTestCase(testResult);
+            }
+            finally
+            {
+                doTearDown(testResult);
+
+                DateTime stop = DateTime.Now;
+                TimeSpan span = stop.Subtract(start);
+                testResult.Time = (double) span.Ticks / (double) TimeSpan.TicksPerSecond;
+
+
+                if (testResult.IsSuccess && this.Properties.Contains("MaxTime"))
+                {
+                    int elapsedTime = (int) Math.Round(testResult.Time * 1000.0);
+                    int maxTime = (int) this.Properties["MaxTime"];
+
+                    if (maxTime > 0 && elapsedTime > maxTime)
+                        testResult.Failure(
+                            string.Format("Elapsed time of {0}ms exceeds maximum of {1}ms",
+                                elapsedTime, maxTime),
+                            null);
+                }
+            }
+        }
+
+        private void doSetUp()
+        {
+            if (setUpMethods != null)
+                foreach (MethodInfo setUpMethod in setUpMethods)
+                    Helper.InvokeMethodDirect(setUpMethod, setUpMethod.IsStatic ? null : this.Fixture);
+        }
+
+        private void doTearDown(TestResult testResult)
+        {
+            if (tearDownMethods != null)
+            {
+                int index = tearDownMethods.Length;
+                while (--index >= 0)
+                    Helper.InvokeMethodDirect(tearDownMethods[index], tearDownMethods[index].IsStatic ? null : this.Fixture);
+            }
+        }
+
+        private void doTestCase(TestResult testResult)
+        {
+            object fixture = this.Method.IsStatic ? null : this.Fixture;
+
+            // Ugh... RunTestMethod is publicly overridable, but depends on a whole bunch of internal/private detail, so it's not really very overridable...
+            var arguments = Helper.ReadPrivateField<object[]>(this, "arguments");
+            var hasExpectedResult = Helper.ReadPrivateField<bool>(this, "hasExpectedResult");
+            var expectedResult = Helper.ReadPrivateField<object>(this, "expectedResult");
+
+            object result = Helper.InvokeMethodDirect(this.Method, fixture, arguments);
+
+            if (hasExpectedResult)
+                NUnitFramework.Assert.AreEqual(expectedResult, result);
+
+            testResult.Success();
+        }
+
+        public override void Run(TestResult testResult)
+        {
+            try
+            {
+                if (this.Parent != null)
+                {
+                    this.Fixture = this.Parent.Fixture;
+                    TestSuite suite = this.Parent as TestSuite;
+                    if (suite != null)
+                    {
+                        this.setUpMethods = suite.GetSetUpMethods();
+                        this.tearDownMethods = suite.GetTearDownMethods();
+                    }
+                }
+
+                // Temporary... to allow for tests that directly execute a test case
+                if (Fixture == null && !Method.IsStatic)
+                    Fixture = Reflect.Construct(this.FixtureType);
+
+                if (this.Properties["_SETCULTURE"] != null)
+                    TestContext.CurrentCulture =
+                        new System.Globalization.CultureInfo((string) Properties["_SETCULTURE"]);
+
+                if (this.Properties["_SETUICULTURE"] != null)
+                    TestContext.CurrentUICulture =
+                        new System.Globalization.CultureInfo((string) Properties["_SETUICULTURE"]);
+
+                int repeatCount = this.Properties.Contains("Repeat")
+                    ? (int) this.Properties["Repeat"] : 1;
+
+                while (repeatCount-- > 0)
+                {
+                    if (RequiresThread || Timeout > 0 || ApartmentState != GetCurrentApartment())
+                        new TestMethodThread(this).Run(testResult, NullListener.NULL, TestFilter.Empty);
+                    else
+                        doRun(testResult);
+
+                    if (testResult.ResultState == ResultState.Failure ||
+                        testResult.ResultState == ResultState.Error ||
+                        testResult.ResultState == ResultState.Cancelled)
+                    {
+                        break;
+                    }
+                }
+
+            }
+            finally
+            {
+                Fixture = null;
+            }
+        }
+    }
+
+    #endregion
+}
---
+++ Src/Helper.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading;
+using RT.Util.ExtensionMethods;
+
+namespace NUnit.Direct
+{
+    public interface IInvokeDirect
+    {
+        object DoInvoke(object instance);
+    }
+
+    static class Helper
+    {
+        private static AssemblyBuilder asmBuilder;
+        private static ModuleBuilder modBuilder;
+        private static Dictionary<string, IInvokeDirect> invokes = new Dictionary<string, IInvokeDirect>();
+
+        public static object InvokeMethodDirect(MethodInfo method, object instance, params object[] parameters)
+        {
+            if (!method.IsStatic && instance == null)
+                throw new ArgumentException("Non-static method requires a non-null instance");
+            if (method.IsStatic && instance != null)
+                throw new ArgumentException("Static method requires a null instance");
+            if (!method.IsStatic && instance.GetType().IsValueType)
+                throw new NotSupportedException("Invoking methods on value types is not yet supported");
+            if (method.IsGenericMethod || method.IsGenericMethodDefinition)
+                throw new NotSupportedException("Generic methods not yet supported");
+
+            if (asmBuilder == null)
+            {
+                asmBuilder = Thread.GetDomain().DefineDynamicAssembly(new AssemblyName("invoke_helper_assembly"), AssemblyBuilderAccess.RunAndSave);
+                modBuilder = asmBuilder.DefineDynamicModule("invoke_helper_module");
+            }
+
+            var sig = getSignature(method);
+            if (!invokes.ContainsKey(sig))
+            {
+                var typeBuilder = modBuilder.DefineType(
+                    "invoke " + sig, TypeAttributes.Public,
+                    typeof(object), new Type[] { typeof(IInvokeDirect) });
+
+                var methodBuilder = typeBuilder.DefineMethod("DoInvoke", MethodAttributes.Public | MethodAttributes.Virtual,
+                    typeof(object), new Type[] { typeof(object) });
+
+                typeBuilder.DefineMethodOverride(methodBuilder, typeof(IInvokeDirect).GetMethod("DoInvoke"));
+
+                var il = methodBuilder.GetILGenerator();
+
+                if (!method.IsStatic)
+                    il.Emit(OpCodes.Ldarg_1);
+                int paramindex = 0;
+                foreach (var param in method.GetParameters())
+                {
+                    il.Emit(OpCodes.Ldarg_2);
+                    EmitLdcI4(il, paramindex);
+                    il.Emit(OpCodes.Ldelem_Ref);
+                    EmitObjectCast(il, param.ParameterType);
+                    paramindex++;
+                }
+                il.Emit(method.IsStatic ? OpCodes.Call : OpCodes.Callvirt, method);
+                if (method.ReturnType == typeof(void))
+                    il.Emit(OpCodes.Ldnull);
+                else if (method.ReturnType.IsValueType)
+                    il.Emit(OpCodes.Box, method.ReturnType);
+                il.Emit(OpCodes.Ret);
+
+                Type type = typeBuilder.CreateType();
+                invokes[sig] = (IInvokeDirect) Activator.CreateInstance(type);
+            }
+
+            return invokes[sig].DoInvoke(instance);
+        }
+
+        private static void EmitLdcI4(ILGenerator il, int value)
+        {
+            switch (value)
+            {
+                case -1: il.Emit(OpCodes.Ldc_I4_M1); break;
+                case 0: il.Emit(OpCodes.Ldc_I4_0); break;
+                case 1: il.Emit(OpCodes.Ldc_I4_1); break;
+                case 2: il.Emit(OpCodes.Ldc_I4_2); break;
+                case 3: il.Emit(OpCodes.Ldc_I4_3); break;
+                case 4: il.Emit(OpCodes.Ldc_I4_4); break;
+                case 5: il.Emit(OpCodes.Ldc_I4_5); break;
+                case 6: il.Emit(OpCodes.Ldc_I4_6); break;
+                case 7: il.Emit(OpCodes.Ldc_I4_7); break;
+                case 8: il.Emit(OpCodes.Ldc_I4_8); break;
+                default:
+                    if (value >= sbyte.MinValue && value <= sbyte.MaxValue)
+                        il.Emit(OpCodes.Ldc_I4_S, (sbyte) value);
+                    else
+                        il.Emit(OpCodes.Ldc_I4, value);
+                    break;
+            }
+        }
+
+        private static void EmitObjectCast(ILGenerator il, Type toType)
+        {
+            if (toType.IsValueType)
+                il.Emit(OpCodes.Unbox_Any, toType);
+            else
+                il.Emit(OpCodes.Castclass, toType);
+        }
+
+        public static T ReadPrivateField<T>(object instance, string name)
+        {
+            return (T) instance.GetType().GetAllFields().First(fi => fi.Name == name).GetValue(instance);
+        }
+
+        private static string getSignature(MethodInfo mi)
+        {
+            if (mi.IsGenericMethod || mi.IsGenericMethodDefinition)
+                throw new NotSupportedException("Generic methods not yet supported");
+            if (mi == null)
+                return "";
+            StringBuilder sb = new StringBuilder();
+
+            if (mi.ReturnType == typeof(void))
+                sb.Append("void ");
+            else
+                sb.Append(mi.ReturnType.FullName + " ");
+
+            sb.Append(mi.DeclaringType.FullName + "::" + mi.Name + "(");
+
+            var ps = mi.GetParameters()
+              .Select(p => p.ParameterType.FullName).ToArray();
+            sb.Append(string.Join(", ", ps));
+            sb.Append(")");
+
+            return sb.ToString();
+        }
+    }
+}
